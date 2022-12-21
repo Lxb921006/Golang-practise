d98c81ae6ec8058fecec6490216b9636a2f212ee
Lxb921006 <120332269@qq.com>
2022-12-21 18:41:25 +0800

    解压.gz文件

--- test/test16/main.go
+++ test/test16/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"compress/gzip"
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	filename := "/Users/liaoxuanbiao/Downloads/log/20221216/2022121618.log.gz"
+
+	gzipfile, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	reader, err := gzip.NewReader(gzipfile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer reader.Close()
 }
