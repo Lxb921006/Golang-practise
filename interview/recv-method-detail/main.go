4e29de6579173930bf0d9667efd1cf14dd718968
Lxb921006 <120332269@qq.com>
2023-02-19 22:20:19 +0800

    方法接收器的细节

--- interview/recv-method-detail/main.go
+++ interview/recv-method-detail/main.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 
-// 方法接收器的类型选择
+// 方法接收器的类型选择: 值无法调用指针方法，指针方法集不可以让值实现接口，存储在接口的值是拿不到地址
+// golang对指针可以隐式的解引用，
 type Adult struct {
 	Name string
 }
 
-func (a *Adult) GetName() {
+func (a Adult) GetName() {
 	fmt.Println(a.Name)
 }
 
