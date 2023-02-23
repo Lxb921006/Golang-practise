7d3f0af7e02f187e8ff203ffec264cc88a1d7beb
Lxb921006 <120332269@qq.com>
2023-02-23 22:16:07 +0800

    chunk streaming

---
+++ interview/byte/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var chunks = make([][]byte, 5)
+	var chunk []byte
+	s := "aabb cc ddv cc dd cc dd 234 asda"
+	b := []byte(s)
+
+	fmt.Println("len(b) = ", len(b))
+
+	for len(b) >= 5 {
+		chunk, b = b[:5], b[5:]
+		chunks = append(chunks, chunk)
+	}
+
+	fmt.Println(chunks)
+
+	if len(b) > 0 {
+		chunks = append(chunks, b[:])
+	}
+
+	fmt.Println(chunks)
+
+	file := "C:/Users/Administrator/Desktop/aa.txt"
+
+	for _, chunk := range chunks {
+		fmt.Println(string(chunk))
+		os.WriteFile(file, chunk, 0777)
+	}
+
+}
