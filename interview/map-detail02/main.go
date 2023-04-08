a10e08c00d2dc3c3e2a000143d7c75d5bd933170
Lxb921006 <120332269@qq.com>
2023-04-08 22:17:26 +0800

    细节

--- interview/map-detail02/main.go
+++ interview/map-detail02/main.go
@@ -15,12 +15,12 @@ func main() {
 
 	sm, ok := m["s01"]
 	if ok {
-		sm.Name = "lqm" //单纯的这样修是不生效的，还得把修改好的sm.Name 重新赋值给sm，如下
+		sm.Name = "lqm" //m := make(map[string]MyStruct)如果map存的是结构体而不是结构体指针，单纯的这样修是不生效的，还得把修改好的sm.Name 重新赋值给sm，如下
 		// m["s01"] = sm
 	}
 
 	//无法获取val的内存地址，它通过哈希对比随机放到不同的bucket
 	// _ = &m["s01"]
 
-	fmt.Println(m["s01"].Name)
+	fmt.Println(m["s01"])
 }
